2422ab4847c3a3819e16d787662a9e389c79547e
MinaTough <mina.toughrani@gmail.com>
2021-08-02 15:16:08 +0200

    stach

--- models/User.js
+++ models/User.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: String,  
-    lastname: String,  
-    email: String,  
-    password: String,
-   admin:Boolean,
-   favorite:{String: Schema.types.ObjectId, ref:"dog_Id" }
+  name: String,
+  lastname: String,
+  email: String,
+  password: String,
+  admin: Boolean,
+  favorite: [{type: Schema.Types.ObjectId, ref: "Pet" }],
 });
 
 const User = mongoose.model("User", userSchema);
