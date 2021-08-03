4e14f139dca4a7ba6faa464a82afb846d996b2d2
MinaTough <mina.toughrani@gmail.com>
2021-08-03 12:16:34 +0200

    Updates

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hbs = require("hbs");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
@@ -25,6 +26,7 @@ hbs.registerPartials(__dirname + "/views/partial");
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,59 @@
-const router = require("express").Router();
+const { Router } = require("express");
+const router = new Router();
+const User = require("../models/User");
+
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+router.get("/signup", (req, res) => res.render("signup"));
+router.get("/userProfile", (req, res) => res.render("user-profile"));
+router.get("/signin", (req, res) => res.render("signin"));
+
+router.post("/signup", (req, res, next) => {
+  console.log("The form data: ", req.body);
+  const { name, email, password } = req.body;
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      //   console.log(`Password hash: ${hashedPassword}`);
+    })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => next(error));
+});
+
+router.post("/signin", (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === "" || password === "") {
+    res.render("signin", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("signin", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        res.render("user-profile", { user });
+      } else {
+        res.render("signin", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
 
 module.exports = router;
