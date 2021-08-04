f18c4ce2be281d2e02614eecdbf1073ffa171cd7
jxxjxxlxx <92627.p@gmail.com>
2021-08-04 14:51:02 +0200

    css update for adoption form

--- models/adoptionForm.js
+++ models/adoptionForm.js
@@ -2,16 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const adoptSchema = new Schema({
-  dogname: String,
-  fullname: String,
-  age: Number,
-  address: String,
-  city: String,
-  homephone: Number,
-  cellphone: Number,
-  email: String,
-  employer: String,
-  city: String,
   household: Boolean,
   caregiver: String,
   housetype: String,
