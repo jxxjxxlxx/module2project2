9a614c903cc9e3f8981eb840475154a876ff99c7
jxxjxxlxx <92627.p@gmail.com>
2021-08-04 14:59:25 +0200

    a push

---
+++ routes/profile.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const Pet = require("../models/Pet");
+const User = require("../models/User");
+const mongoose = require('mongoose');
+const upload= require ("../config/cloudinary.config");
+
+
+router.get("/user/:id/user-profile", (req, res, next)=>{
+    Pet.findById(req.params.id)
+    .then((dogDoc)=>{
+
+        res.render("user-profile.hbs", {doggy : dogDoc})
+    })
+    .catch(e=>console.log(e))
+})
+module.exports = router;
